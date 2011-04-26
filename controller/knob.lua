c04ba3b21cfa2a604ab2718d738649cd652b3921
Angus Gratton <gus@projectgus.com>
2011-04-26 20:49:06 +1000

    Restructure into a poor man's bodgy FSM code

--- controller/knob.lua
+++ controller/knob.lua
@@ -3,6 +3,8 @@
 require "probe"
 
 knob = {}
+knob.states = { STILL=nil, MOVING=nil } -- to become function lookup table
+knob.state = "STILL"
 
 local last_knob_still_pos = nil
 local last_knob_moving_pos = nil
@@ -20,31 +22,61 @@ local function get_knob_movement()
 		last_knob_still_pos = raw
 		last_knob_moving_pos = last_knob_still_pos
 		last_knob_movetime = os.time()
+		return nil
 	end
 
-	if raw == nil or math.abs(raw - last_knob_still_pos) < config.knob_deadband then 	
+	if raw == nil then -- offline
+		knob.state = "STILL"
+		return nil
+	else
+		-- call appropriate handler (stored in knob.states table)
+		return knob.states[knob.state](raw)
+	end
+end
+
+-- handle a new raw value when still
+local function knob_handle_still(raw)
+	if math.abs(raw - last_knob_still_pos) < config.knob_deadband then 	
 		return nil -- offline or hasn't moved from last resting spot
-	end
+	else
+		-- check it's not a blip
+		raw2 = probe.get_position()
+		if raw2 ~= raw then
+			return nil -- was an ADC blip
+		end
+		knob.state = "MOVING"
+		probe.fast_green_blink() -- on the move so start blinking
+		last_knob_moving_pos = raw
+		last_knob_movetime = os.time()		
+		return nil
+	end	
+end
 
-	probe.fast_green_blink() -- on the move so start blinking
+-- handle a new raw value when moving
+local function knob_handle_moving(raw)
+
+	-- feedback on the dial in realtime
+	probe.set_dial(round(translate(translate(raw, config.knob_table), config.dial_table)))
 
 	if math.abs(raw - last_knob_moving_pos) > config.knob_deadband then		      
-		last_knob_still_pos = -10000 -- even if it goes back where it came from, counts as a move now
 		last_knob_moving_pos = raw
 		last_knob_movetime = os.time()
 		return nil -- still on the move
 	end
-	
+
 	if os.time() < last_knob_movetime + config.knob_latency then
 		return nil -- hasn't settled for long enough yet
 	end 
 
-	-- congrats, knob has moved!
+	-- congrats, movement completed!
 	log("Knob has moved to new raw position " .. raw)
+	knob.state = "STILL"
 	last_knob_moving_pos = raw
 	last_knob_still_pos = raw
 	return translate(raw, config.knob_table)
 end
 
+
+knob.states = { STILL=knob_handle_still, MOVING=knob_handle_moving }
 
 knob.get_movement = get_knob_movement
